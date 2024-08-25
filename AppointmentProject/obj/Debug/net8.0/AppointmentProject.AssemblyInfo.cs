30e3b56453c1c7ad12bff68247b726bcf5e0727c
MegaVlr <159097631+MegaVlr@users.noreply.github.com>
2024-08-25 20:53:21 +0300

    Fix Remove .vs files from repository

--- AppointmentProject/obj/Debug/net8.0/AppointmentProject.AssemblyInfo.cs
+++ AppointmentProject/obj/Debug/net8.0/AppointmentProject.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AppointmentProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77c3fa2458f5a4b7e8a64b3a014f6f4ea8ad3fab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d909d744575189a6a2e09ace742916df1bbb694c")]
 [assembly: System.Reflection.AssemblyProductAttribute("AppointmentProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AppointmentProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
